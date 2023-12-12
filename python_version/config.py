033441f92c9a0cd8ad6fb43c6dfb7169c03afc00
HaolanDuan <duanhaolan@gmail.com>
2023-12-12 22:07:35 +0800

    converted config

--- python_version/config.py
+++ python_version/config.py
@@ -1,7 +1,17 @@
 import os
+import mylib
+import config
+
+config = config.Config()
+result = config.Result()
 
 def exists_test(name):
     if os.path.isfile(name):
         return True
     else:
         return False
+
+def assert_file_exist(desc, name):
+    if not exists_test(name):
+        print(desc + " " + name + "not found")
+        exit(1)
